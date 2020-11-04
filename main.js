6031e2625843ccd9b1559f9eadcef4a3ae739e82
jeffreyotan <jeffreyotan@yahoo.com>
2020-11-04 14:15:21 +0800

    Improved aesthetics and make it Heroku ready

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const handlebars = require('express-handlebars');
 const mysql = require('mysql2/promise');
 
 // configure the port
-const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000;
 
 // SQL commands
 const SQL_QUERY_TV_NAMES_DESC = "select tvid, name from tv_shows order by name desc limit ?";
 const SQL_GET_TV_DETAILS_BY_ID = "select * from tv_shows where tvid like ?";
 
 // define global constants
-const QUERYLIMIT = 20;
+const QUERYLIMIT = parseInt(process.env.DB_QUERY_LIMIT) || 50;
 
 // create an instance of express
 const app = express();
@@ -29,6 +29,26 @@ const pool = mysql.createPool({
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 4,
     timezone: '+08:00'
 });
+
+// creating specific helper query functions using the concept of closures.. simplify calling process
+const mkQuery = (sqlStmt, dbPool) => {
+    const f = async (params) => {
+        const conn = await dbPool.getConnection();
+
+        try {
+            const results = await conn.query(sqlStmt, params);
+            return results[0];
+        } catch (e) {
+            return Promise.reject(e);
+        } finally {
+            conn.release();
+        }
+    }
+    return f;
+}
+
+const getTVList = mkQuery(SQL_QUERY_TV_NAMES_DESC, pool);
+const getTVDetails = mkQuery(SQL_GET_TV_DETAILS_BY_ID, pool);
 
 const startApp = async (newApp, newPool) => {
     try {
@@ -50,11 +70,11 @@ const startApp = async (newApp, newPool) => {
 };
 
 app.get('/', async (req, res, next) => {
-    const conn = await pool.getConnection();
+    // const conn = await pool.getConnection();
 
     try {
-        const results = await conn.query(SQL_QUERY_TV_NAMES_DESC, [QUERYLIMIT]);
-        const tvPrograms = results[0];
+        // const results = await conn.query(SQL_QUERY_TV_NAMES_DESC, [QUERYLIMIT]);
+        const tvPrograms = await getTVList([QUERYLIMIT]);
 
         res.format({
             default: () => {
@@ -66,17 +86,17 @@ app.get('/', async (req, res, next) => {
         console.error('Could not load landing page. Error: ', e);
         res.status(500).type('text/html');
         res.send('<h2>Unable to load landing page due to an Internal Server error!</h2>');
-    } finally {
+    }/* finally {
         conn.release();
-    }
+    }*/
 });
 
 app.get('/app/:tvid', async (req, res, next) => {
-    const conn = await pool.getConnection();
+    // const conn = await pool.getConnection();
 
     try {
-        const results = await conn.query(SQL_GET_TV_DETAILS_BY_ID, [req.params['tvid']]);
-        const tvShow = results[0];
+        // const results = await conn.query(SQL_GET_TV_DETAILS_BY_ID, [req.params['tvid']]);
+        const tvShow = await getTVDetails([req.params['tvid']]);
 
         // console.info("details of TV SHow is ", tvShow);
 
@@ -90,9 +110,9 @@ app.get('/app/:tvid', async (req, res, next) => {
         console.error(`Unable to load TV with TVID: ${req.params['tvid']}`);
         res.status(404).type('text/html');
         res.send(`<h2>Unable to load page with TVID: ${req.params['tvid']}`);
-    } finally {
+    }/* finally {
         conn.release();
-    }
+    }*/
 });
 
 startApp(app, pool);
